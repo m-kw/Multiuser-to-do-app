0325d8556e6cfb14702c2615858dc00d194304df
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-20 12:24:04 +0100

    Add imput's value and add it to the component's state

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import io from 'socket.io-client';
 
-const Form = () => {
-  return (
-    <form id="add-task-form">
-      <input className="text-input" autoComplete="off" type="text" placeholder="Add new task" id="task-name" />
-      <button className="btn" type="submit">Add</button>
-    </form>
-  )
+class Form extends React.Component {
+  state = {
+    taskName: '',
+  }
+
+  componentDidMount() {
+    this.socket = io('http://localhost:8000');
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    this.addTask(this.state.taskName);
+    this.socket.emit('addTask', this.state.taskName);
+  }
+
+  render() {
+    const { taskName } = this.state;
+
+    return (
+      <form id="add-task-form" onSubmit={() => this.submitForm()}>
+        <input
+          className="text-input"
+          autoComplete="off"
+          type="text"
+          placeholder="Add new task"
+          id="task-name"
+          value={taskName}
+          onChange={(e) => this.setState({ taskName: e.target.value })}
+        />
+        <button className="btn" type="submit">Add</button>
+      </form>
+    )
+  }
 };
 
 export default Form;
