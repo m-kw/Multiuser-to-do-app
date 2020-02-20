df72e618205740a74eae681870e8cfb9d5b4a39e
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-20 10:26:41 +0100

    Add basic to-do-list structure to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
 class App extends React.Component {
-  
+
   render() {
     return (
       <div className="App">
-        Hello world
+
+        <header>
+          <h1>To-do-app</h1>
+        </header>
+
+        <section className="tasks">
+          <h2>Tasks</h2>
+
+          <ul className="tasks-list">
+            <li className="task">
+              Shopping
+              <button class="btn">Remove</button>
+            </li>
+            <li className="task">
+              Go for a walk
+              <button class="btn">Remove</button>
+            </li>
+          </ul>
+
+          <form id="add-task-form">
+            <input className="text-input" autocomplete="off" type="text" placeholder="Add new task" id="task-name" />
+            <button className="btn" type="submit">Add</button>
+          </form>
+        </section>
+
       </div>
     );
   };
